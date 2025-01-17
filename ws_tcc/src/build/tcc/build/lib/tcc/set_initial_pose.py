90d76fd82ae87350ad4d92e91517cec99b1ed927
Lucas Agostinho Silva <lucasagostinhosilv@gmail.com>
2025-01-16 23:46:21 -0300

    fixing MPPI and RPP

---
+++ ws_tcc/src/build/tcc/build/lib/tcc/set_initial_pose.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node
+from nav2_simple_commander.robot_navigator import BasicNavigator
+from geometry_msgs.msg import PoseStamped
+import tf_transformations
+
+class set_initial_pose(Node):
+
+    def __init__(self):
+        super().__init__('set_initial_pose')
+
+        self.navigator = BasicNavigator()
+
+        initial_pose = self.create_pose_stamped(3.1, 1.1, 3.14)
+        self.navigator.setInitialPose(initial_pose)
+
+        self.navigator.waitUntilNav2Active()
+
+
+    def create_pose_stamped(navigator: BasicNavigator, position_x, position_y, orientation_z):
+        q_x, q_y, q_z, q_w = tf_transformations.quaternion_from_euler(0.0, 0.0, orientation_z)
+        pose = PoseStamped()
+        pose.header.frame_id = 'map'
+        pose.header.stamp = navigator.get_clock().now().to_msg()
+        pose.pose.position.x = position_x
+        pose.pose.position.y = position_y
+        pose.pose.position.z = 0.0
+        pose.pose.orientation.x = q_x
+        pose.pose.orientation.y = q_y
+        pose.pose.orientation.z = q_z
+        pose.pose.orientation.w= q_w
+        return pose
+
+def main(args=None):
+    #init
+    rclpy.init(args=args)
+    node = set_initial_pose()
+
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        # Shutdown node
+        node.navigator.getLogger().info("Shutting down checkpoint node...")
+        rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
---
+++ ws_tcc/src/build/tcc/prefix_override/sitecustomize.py
@@ -0,0 +1,4 @@
+import sys
+if sys.prefix == '/usr':
+    sys.real_prefix = sys.prefix
+    sys.prefix = sys.exec_prefix = '/home/lucas/Documents/GitHub/TCC-robo-limpeza-de-linha/ws_tcc/src/install/tcc'
